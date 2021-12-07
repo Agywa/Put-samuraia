96ff1bf5ef333d81a6738d927b8a086af3a1db18
Agywa <91954956+Agywa@users.noreply.github.com>
2021-12-07 21:25:29 +0300

    Initialize project using Create React App

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import s from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className={s.nav}>
+            <div>
+                <NavLink to="/profile" className={({isActive}) => isActive ? s.activeNot : s.activeDat}> Profile </NavLink>
+            </div>
+            <div>
+                <NavLink to="dialogs" className={({isActive}) => isActive ? s.activeNot : s.activeDat}> Messages </NavLink>
+            </div>
+            <div>
+                <NavLink to="news" className={({isActive}) => isActive ? s.activeNot : s.activeDat}> News </NavLink>
+            </div>
+            <div>
+                <NavLink to="music" className={({isActive}) => isActive ? s.activeNot : s.activeDat}> Music </NavLink>
+            </div>
+            <div>
+                <NavLink to="settings" className={({isActive}) => isActive ? s.activeNot : s.activeDat}> Settings </NavLink>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar

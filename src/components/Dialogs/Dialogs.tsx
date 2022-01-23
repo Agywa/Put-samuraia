1b1d2783802a7b0fb9540ab1299b37ba2f826e00
Agywa <91954956+Agywa@users.noreply.github.com>
2022-01-23 11:06:30 +0300

    21/01.22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,7 +5,6 @@ import DialogItem from "./DialogItem/DialogItem";
 import {DialogType, MessageType} from "../../redux/store";
 
 
-
 type DialogsType = {
     messagePage: Array<MessageType>
     dialogsPage: Array<DialogType>

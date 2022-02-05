a8e0cf6958690b711239047ddda7149b258bf778
Agywa <91954956+Agywa@users.noreply.github.com>
2022-02-05 12:59:51 +0300

    21/01.22

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,22 +6,19 @@ import axios from "axios";
 import userPhoto from "../../assets/images/user.png"
 
 
-class Users extends React.Component<UsersPropsType, any>{
-
-     getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get(" https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                });
-        }
+class Users extends React.Component<UsersPropsType, any> {
+    constructor(props: UsersPropsType) {
+        super(props);
+        axios.get(" https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
 
     render() {
         return <div>
-            <button onClick={this.getUsers}>Get Users</button>
             {
-               this.props.users.map((u: UserType) => <div key={u.id}>
+                this.props.users.map((u: UserType) => <div key={u.id}>
                 <span>
                     <div>
                     <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto}/>

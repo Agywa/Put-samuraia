dbbe2bed176574f10106b4a7e56a867b31fbcefd
Agywa <91954956+Agywa@users.noreply.github.com>
2022-02-11 21:14:47 +0300

    21/01.22

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -9,10 +9,45 @@ import {
 } from "../../redux/users_reduser";
 import {Dispatch} from "redux";
 import {connect} from "react-redux";
-import UsersAPIComponent from "./UsersAPIComponent";
 import {AppStateType} from "../../redux/redux-store";
+import axios from "axios";
+import {Users} from "./Users";
 
+class UsersContainer extends React.Component<UsersPropsType, any> {
+    constructor(props: UsersPropsType) {
+        super(props);
+    }
 
+    componentDidMount() {
+        axios.get(` https://social-network.samuraijs.com/api/1.0/users`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+
+            });
+    }
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(` https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
+
+    render() {
+
+        return <Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+
+        />
+    }
+}
 
 type MapStateToPropsType = {
     users: Array<UserType>
@@ -59,6 +94,5 @@ let mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
         }
     }
 }
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
 
-export default UsersContainer
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
